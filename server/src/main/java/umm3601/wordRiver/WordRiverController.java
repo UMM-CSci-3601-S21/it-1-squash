f123fd915901186257974bbb75d33e6621a17aeb
Jacob <jenne077@morris.umn.edu>
2021-03-14 16:18:08 -0500

    Remove unneeded imports
    Removed some of the import statements that were not needed, and made some of the code a little prettier

--- server/src/main/java/umm3601/wordRiver/WordRiverController.java
+++ server/src/main/java/umm3601/wordRiver/WordRiverController.java
@@ -1,45 +1,31 @@
 package umm3601.wordRiver;
 
-import static com.mongodb.client.model.Filters.eq;
-
-
 import java.util.ArrayList;
-
-
-import com.google.common.collect.ImmutableMap;
 import com.mongodb.client.MongoDatabase;
-
 import org.bson.Document;
-import org.bson.types.ObjectId;
 import org.mongojack.JacksonMongoCollection;
-
-import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
-import io.javalin.http.NotFoundResponse;
+
 public class WordRiverController {
 
-  private final JacksonMongoCollection<ContextPack> ctxCollection;
+  private final JacksonMongoCollection<ContextPack> contextPackCollection;
 
+  /**
+   * Construct a controller for context packs
+   *
+   * @param database the database containing context pack data
+   */
+  public WordRiverController(MongoDatabase database) {
+    contextPackCollection = JacksonMongoCollection.builder().build(database, "wordlists", ContextPack.class);
+  }
 
-/**
- * Construct a controller for context packs
- *
- * @param database the database containing context pack data
- */
-
- public WordRiverController(MongoDatabase database) {
-   ctxCollection = JacksonMongoCollection.builder().build(database, "wordlists", ContextPack.class);
- }
-
-
- /**
-  * Get a Json response with a list of all context packs
-  *
-  * @param ctx a Javalin HTTP context
-  */
-
+  /**
+   * Get a Json response with a list of all context packs
+   *
+   * @param ctx a Javalin HTTP context
+   */
   public void getPacks(Context ctx) {
-    ctx.json(ctxCollection.find(new Document()).into(new ArrayList<>()));
+    ctx.json(contextPackCollection.find(new Document()).into(new ArrayList<>()));
   }
 
 }
