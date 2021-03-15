557a03abc105f49f856bd2b718df92d1490b928e
Josh Quist <quist127@morris.umn.edu>
2021-03-15 16:40:26 -0500

    Implement getPack() and tests
    Implemented the getPack() method in WordRiverController and tested it. This should also fix the test coverage problem that kept us from merging the branch containing the user code.

--- server/src/main/java/umm3601/wordRiver/WordRiverController.java
+++ server/src/main/java/umm3601/wordRiver/WordRiverController.java
@@ -1,10 +1,17 @@
 package umm3601.wordRiver;
 
+import static com.mongodb.client.model.Filters.eq;
+
 import java.util.ArrayList;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 import org.mongojack.JacksonMongoCollection;
+
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
 
 public class WordRiverController {
 
@@ -26,6 +33,23 @@ public class WordRiverController {
    */
   public void getPacks(Context ctx) {
     ctx.json(contextPackCollection.find(new Document()).into(new ArrayList<>()));
+  }
+
+  public void getPack(Context ctx) {
+
+    String id = ctx.pathParam("id");
+    ContextPack contextPack;
+
+    try {
+      contextPack = contextPackCollection.find(eq("_id", new ObjectId(id))).first();
+    } catch (IllegalArgumentException e) {
+      throw new BadRequestResponse("The requested context pack id wasn't a legal Mongo Object ID.");
+    }
+    if (contextPack == null) {
+      throw new NotFoundResponse("The requested context pack was not found");
+    } else {
+      ctx.json(contextPack);
+    }
   }
 
 }
